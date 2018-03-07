e819a4979ed5a313a4525d93e9bf63f94f00c027
sevic69 <sebdeveloper6952@gmail.com>
2018-03-07 15:33:13 -0600

    Agregar proyecto netbeans.

---
+++ hdt6/src/hdt6/CControlador.java
@@ -0,0 +1,12 @@
+package hdt6;
+
+/**
+ *
+ * @author Jose Andres Arenas 14470
+ * @author Sebastian Arriola 11463
+ * @author Fernando Figueroa 14175
+ */
+public class CControlador 
+{
+    
+}
---
+++ hdt6/src/hdt6/CModelo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hdt6;
+
+/**
+ *
+ * @author sevic69
+ */
+public class CModelo {
+    
+}
