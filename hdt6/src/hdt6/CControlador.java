5969a0a7dd26f41e5bb72e0965f6d6ebf80743b9
JoseArenas <jo.arenas96@gmail.com>
2018-03-08 19:36:32 +0000

    comentarios de clase controlador

--- hdt6/src/hdt6/CControlador.java
+++ hdt6/src/hdt6/CControlador.java
@@ -7,12 +7,25 @@ import javax.swing.JFrame;
  * @author Jose Andres Arenas 14470
  * @author Sebastian Arriola 11463
  * @author Fernando Figueroa 14175
+ * 
+ * 
+ * Esta clase es la que interpreta los inputs del usuario, como los clicks en
+ * los botones. Es el objeto que sirve como puente entre la clase vista y la 
+ * clase modelo.
  */
+
+
+
+
 public class CControlador 
 {
     protected CModelo modelo;
     protected JFrame vista;
     
+    /**
+     * Este es el constructor de la clase. Se encarga de instanciar el modelo, 
+     * y hacer visible la vista.
+     */
     public CControlador()
     {
         modelo = new CModelo();
@@ -20,11 +33,24 @@ public class CControlador
         f.setVisible(true);
     }
     
+    
+    /**
+     * Recibe el input del boton agregar, y lo envia a la clase modelo.
+     * @param nombre El texto dentro del jTextfield que corresponde al nombre
+     * de la carta que se quiere agregar. 
+     */
+    
     public void agregarCartaAColeccion(String nombre)
     {
         modelo.agregarCarta(nombre);
     }
     
+    
+    /**
+     * Recibe el input del boton buscar y lo envia a la clase modelo.
+     * @param nombre El texto dentro del jTextfield que corresponde al nombre
+     * de la carta que se quiere agregar. 
+     */
     public void buscarCarta(String nombre)
     {
         modelo.buscarTipo(nombre);
